8ede120129543ef8050096cebb73702e1fab976a
Dhemes Mota <dhemes.mota@gmail.com>
2024-05-07 09:06:51 -0300

    feat: generate pdf

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import path from 'path';
 import pdfRoutes from './routes/pdfRoutes';
 
 const app = express();
 const port = 3333;
 
+
+app.use(cors()); 
+
 app.use(bodyParser.json());
 app.use(pdfRoutes);
+
+app.use('/files', express.static(path.join(__dirname, '../files')));
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
--- src/services/pdfService.ts
+++ src/services/pdfService.ts
@@ -1,19 +1,35 @@
 import fs from 'fs';
+import path from 'path';
 import PDFDocument from 'pdfkit';
 
 export const generatePDF = async (processNumber: number, propertyDescription: string, postalCode: string): Promise<string> => {
     return new Promise((resolve, reject) => {
         const doc = new PDFDocument();
-        const fileName = `files/${processNumber}_${Date.now()}.pdf`;
-        const filePath = `${__dirname}/../../${fileName}`;
+
+        // Caminho para a pasta files na raiz do projeto
+        const filePath = path.join(__dirname, '../../files');
+
+        // Certifique-se de que a pasta existe, se não, crie-a
+        if (!fs.existsSync(filePath)) {
+            fs.mkdirSync(filePath, { recursive: true });
+        }
+
+        // Nome do arquivo
+        const fileName = `${processNumber}_${Date.now()}.pdf`;
+        const fileFullPath = path.join(filePath, fileName);
         
-        doc.pipe(fs.createWriteStream(filePath));
+        const writeStream = fs.createWriteStream(fileFullPath);
+        doc.pipe(writeStream);
         doc.fontSize(12).text(`Número do Processo: ${processNumber}`);
         doc.text(`Descrição da Propriedade: ${propertyDescription}`);
         doc.text(`Código Postal: ${postalCode}`);
         doc.end();
 
-        doc.on('finish', () => resolve(`http://localhost:3000/${fileName}`));
-        doc.on('error', reject);
+        writeStream.on('close', () => {
+            const fileUrl = `http://localhost:3333/files/${fileName}`;
+            resolve(fileUrl);
+        });
+
+        writeStream.on('error', reject);
     });
 };
